5306b92aca44fa470f5d73781abab31d4580bf46
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-01-05 07:39:19 +0600

    init cloudhr

---
+++ src/router/router.js
@@ -0,0 +1,16 @@
+const routes = [
+  {
+    label: "Dashboard",
+    icon: 'chart-pie'
+  },
+  {
+    label: 'Add Employee',
+    icon: 'chart-pie'
+  },
+  {
+    label: "All Employees",
+    icon: 'chart-pie'
+  }
+]
+
+export default routes
