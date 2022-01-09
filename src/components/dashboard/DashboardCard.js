841404698271130060c0c4d470021a9d2a025948
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-01-10 03:38:11 +0600

    dashboard card design issue fixed

--- src/components/dashboard/DashboardCard.js
+++ src/components/dashboard/DashboardCard.js
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 const DashboardCard = ({ value, title, variant }) => {
 	return (
 		<Card className={`bg-${variant}`}>
-			<Card.Body className='d-flex flex-center'>
+			<Card.Body className='d-flex flex-wrap flex-center'>
 				<h2 className='text-white mb-0 me-3'>{value}</h2>
 				<p className='mb-0 text-white fw-medium'>{title}</p>
 			</Card.Body>
