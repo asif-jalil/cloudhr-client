d231a3f9b1430c275d3ee11c00e647476151270a
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-01-09 16:59:28 +0600

    search added

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -1,62 +1,63 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Pagination, Table } from "react-bootstrap";
-import usePagination from "../../hooks/usePagination";
-import Topbar from "../navbar/Topbar";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Pagination, Table } from 'react-bootstrap';
+import usePagination from '../../hooks/usePagination';
+import Topbar from '../navbar/Topbar';
 
 const Employees = () => {
-  const [employees, setEmployees] = useState([]);
-  const [totalEmployee, setTotalEmployee] = useState(0);
-  const { next, prev, currentPage } = usePagination(totalEmployee);
+	const [employees, setEmployees] = useState([]);
+	const [totalEmployee, setTotalEmployee] = useState(0);
+	const { next, prev, currentPage } = usePagination(totalEmployee);
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await axios.get("http://localhost:5000/employees/count");
-      setTotalEmployee(res.data);
-    }
+	useEffect(() => {
+		async function fetchData() {
+			const res = await axios.get('http://localhost:5000/employees/count');
+			setTotalEmployee(res.data);
+		}
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/employees/view?page=${currentPage}`)
-      .then((res) => {
-        setEmployees(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [currentPage]);
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/employees/view?page=${currentPage}`)
+			.then(res => {
+				setEmployees(res.data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, [currentPage]);
 
-  return (
-    <>
-      <Topbar title="Employees" />
+	return (
+		<>
+			<Topbar title='Employees' />
 
-      <Table bordered className="border-300">
-        <thead className="bg-200">
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee) => (
-            <tr key={employee.id}>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Pagination>
-        <Pagination.Prev onClick={prev}>Prev</Pagination.Prev>
-        <Pagination.Next onClick={next}>Next</Pagination.Next>
-      </Pagination>
-    </>
-  );
+			<Table bordered className='border-300'>
+				<thead className='bg-200'>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					{employees.map(employee => (
+						<tr key={employee.id}>
+							<td>{employee.firstName}</td>
+							<td>{employee.lastName}</td>
+							<td>{employee.email}</td>
+						</tr>
+					))}
+				</tbody>
+			</Table>
+			<Pagination>
+				<Pagination.Prev onClick={prev}>Prev</Pagination.Prev>
+				<Pagination.Next onClick={next}>Next</Pagination.Next>
+			</Pagination>
+			<small>Showing 5 Results of {totalEmployee}</small>
+		</>
+	);
 };
 
 export default Employees;
--- src/components/navbar/Topbar.js
+++ src/components/navbar/Topbar.js
@@ -1,29 +1,41 @@
-import React, { useState } from "react";
-import { Col, Dropdown, Form, Row } from "react-bootstrap";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Col, Dropdown, Form, Row } from 'react-bootstrap';
 
 const Topbar = ({ title }) => {
-  const [show, setShow] = useState(false);
-  return (
-    <Row className="justify-content-between mb-5 mb-sm-4">
-      <Col className="mb-4">
-        <h4 className="ms-6 ms-xl-0">{title}</h4>
-      </Col>
-      <Col sm={6} md={5} lg={4} xl={3}>
-        <Dropdown onToggle={() => setShow(!show)}>
-          <Dropdown.Toggle as="div" data-toggle="dropdown" bsPrefix="toggle">
-            <Form>
-              <Form.Control type="text" className="rounded-pill" placeholder="Search Employee" />
-            </Form>
-          </Dropdown.Toggle>
-          <Dropdown.Menu className="w-100">
-            <div className="scrollbar py-2" style={{ maxHeight: "20rem" }}>
-              <h6 className="mb-0 px-3">Nothing to show</h6>
-            </div>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Col>
-    </Row>
-  );
+	const [show, setShow] = useState(false);
+	const [message, setMessage] = useState('Nothing to show');
+
+	const handleSearch = async searchTerm => {
+		if (searchTerm.length >= 1) {
+			const res = await axios.get(`http://localhost:5000/employees/search?searchTerm=${searchTerm}`);
+			console.log(res);
+		} else {
+			setMessage('Type at least 3 letter to show result');
+		}
+	};
+
+	return (
+		<Row className='justify-content-between mb-5 mb-sm-4'>
+			<Col className='mb-4'>
+				<h4 className='ms-6 ms-xl-0'>{title}</h4>
+			</Col>
+			<Col sm={6} md={5} lg={4} xl={3}>
+				<Dropdown onToggle={() => setShow(!show)}>
+					<Dropdown.Toggle as='div' data-toggle='dropdown' bsPrefix='toggle'>
+						<Form>
+							<Form.Control type='text' onChange={e => handleSearch(e.target.value)} className='rounded-pill' placeholder='Search Employee' />
+						</Form>
+					</Dropdown.Toggle>
+					<Dropdown.Menu className='w-100'>
+						<div className='scrollbar py-2' style={{ maxHeight: '20rem' }}>
+							<h6 className='mb-0 px-3'>{message}</h6>
+						</div>
+					</Dropdown.Menu>
+				</Dropdown>
+			</Col>
+		</Row>
+	);
 };
 
 export default Topbar;
